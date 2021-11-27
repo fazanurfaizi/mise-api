392998d39bd6ef7703ba68c658cbcee1742d50be
Razor <nurfaizi.faza@gmail.com>
2021-11-27 10:04:24 +0700

    feature: create migration

---
+++ database/migrations/2021_11_27_023158_create_configurations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configurations', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique();
+            $table->string('display_name');
+            $table->text('value')->nullable();
+            $table->text('details')->nullable();
+            $table->string('type');
+            $table->integer('order')->default(1);
+            $table->string('group')->nullable();
+            $table->boolean('deletable')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configurations');
+    }
+}
