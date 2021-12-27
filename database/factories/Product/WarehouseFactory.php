0737684d1b99c67f1c9ba4e291c82b45d7d914e6
Razor <nurfaizi.faza@gmail.com>
2021-12-27 08:54:23 +0700

    Product Warehouse Factory

---
+++ database/factories/Product/WarehouseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\Product;
+
+use App\Models\Product\Warehouse;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WarehouseFactory extends Factory
+{
+    protected $model = Warehouse::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->words(3, true)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product\ProductVariant;
+use App\Models\Product\Warehouse;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User\User::factory(10)->create();
-        ProductVariant::factory(10)->create();
+        // ProductVariant::factory(10)->create();
+        Warehouse::factory(10)->create();
     }
 }
